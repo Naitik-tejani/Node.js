96af7181734f108d08bc05ad3f1389aa19c58549
Naitik-tejani <7133naititejani.com>
2024-12-16 10:45:34 +0530

    first commit

---
+++ lec-1.1/index.js
@@ -0,0 +1,16 @@
+console.log('he is a good boy'); 
+
+const myObject = {  
+    name: 'John',
+    age: 30,
+    occupation: 'Developer',
+    hobbies: ['reading', 'swimming', 'coding'],
+    address: {
+        street: '123 Main St',
+        city: 'Anytown',
+        state: 'CA',
+        zip: '12345'
+        }
+        };
+        console.log(myObject);
+        
