1740cbb0dc71b569c4a06f84c8b49b2902c3cd02
Naitik-tejani <7133naititejani.com>
2024-12-23 12:02:03 +0530

    add file

--- pr-1/server.js
+++ pr-1/server.js
@@ -8,19 +8,28 @@ const server = http.createServer((req, res) => {
         case "/":
             fileName = "./home.html";
             break;
-            case "/about":
+        case "/About":
             fileName = "./about.html";
             break;
+        case "/product":
+            fileName = "./product.html";
+            break;
+        case "/contact":
+            fileName = "./contact.html";
+            break;
+        case "/blog":
+            fileName = "./blog.html";
+            break;
     }
-    fs.readFile(fileName,(err, pagename)=> {
-    if (err) {
-        console.log('file not found');
-        return false;
-    }
-    res.end(pagename)
-});
-     })
-     
+    fs.readFile(fileName, (err, pagename) => {
+        if (err) {
+            console.log('file not found');
+            return false;
+        }
+        res.end(pagename);
+    });
+})
+
 server.listen(port, (err) => {
     if (err) {
         console.log(`server is start on port :- ${port}`);
