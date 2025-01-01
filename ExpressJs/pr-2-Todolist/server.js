6895d52176b1b19b504f376301cdcd85bbb9f997
Naitik-tejani <7133naititejani.com>
2025-01-01 09:23:32 +0530

    add file

---
+++ ExpressJs/pr-2-Todolist/server.js
@@ -0,0 +1,44 @@
+const express=require('express'); 
+const port=8060;
+const app =express();
+
+app.set('view engine','ejs');
+app.use(express.urlencoded());
+let record=[];
+
+app.get('/',(req,res)=>{
+    return res.render('Table',{
+        record
+    });
+});
+
+app.get('/form',(req,res)=>{
+    return res.render('Form');
+});
+
+app.post('/adduser',(req,res)=>{
+    const {username,userphone}=req.body;
+    let obj={
+        name:username,
+        phone:userphone
+    }
+    record.push(obj);
+    console.log("user sucesfully register");
+    return res.redirect('/');
+})
+
+app.get('/deletuser',(req,res)=>{
+   let id =req.query.deletId
+   let deletdata = record.filter(val=>val.id !=id);
+   record=deletdata;
+   return res.redirect('/');
+})
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log('server is strat on port http://localhost:${port}');
+})
