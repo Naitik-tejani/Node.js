322accfd2f8a49534f54ca70df90b66ec0a90ae4
Naitik-tejani <7133naititejani.com>
2025-04-03 23:47:01 +0530

    add file

--- PR-9 API/config/db.js
+++ PR-9 API/config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/PR-9-blog');
+
+mongoose.connect('mongodb://127.0.0.1/pr-9-Blog');
 
 const db = mongoose.connection;
 
-db.on("connected",(err)=>{
+db.on("connected", (err) => {
     if (err) {
         console.log(err);
         return false
     }
-    console.log("DATABSE CONNECTED SUCCESSFULLY..!");
+    console.log('database successfully connected');
 })
+
 module.exports = db;
